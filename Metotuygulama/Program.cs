0b1b7b77f4bc9e68f9f2d4fb6cf50320011212d2
Esra Alkan <es.alknn@gmail.com>
2021-11-26 02:43:57 +0300

    Add project files.

---
+++ Metotuygulama/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotuygulama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dizi = { 1, 5, 42, 89, 43, 7, 86, 58 };
+            int a=topla(dizi);
+            Console.WriteLine($"Dizi elemanlarının toplamı : {a}");
+            int b = ortalama(dizi);
+            Console.WriteLine($"Dizi elemanlarının ortalaması : {b}");
+            Console.ReadLine();
+
+        }
+
+        public static int topla(int[] toplama)
+        {
+            int toplam = 0;
+            for (int i = 0; i < toplama.Length; i++)
+            {
+                toplam += toplama[i];
+            }
+            return toplam;
+        }
+
+        public static int ortalama(int[] ort)
+        {
+            int ortalama = 0;
+            int toplam = 0;
+            for (int i = 0; i < ort.Length; i++)
+            {
+                toplam += ort[i];
+                ortalama = (int)toplam / (ort.Length);
+            }
+            return ortalama;
+        }
+    }
+}
---
+++ freeapplications/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace freeapplications
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen vize notunu giriniz: ");
+            int vize = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Lütfen final notunu giriniz: ");
+            int final = Convert.ToInt32(Console.ReadLine());
+            int a=durum(vize, final);
+            Console.WriteLine(a);
+            Console.ReadLine();
+
+
+           
+
+
+        }
+        static int durum(int not1,int not2)
+        {
+            int ort = (not1 + not2) / 2;
+            if (ort<100 && ort>45)
+            {
+                Console.WriteLine($"Tebrikler dersi geçtiniz ortalamanız {ort}");
+            }
+            else
+            {
+                Console.WriteLine($"Dersden kaldınız ortalamanız {ort}");
+            }
+            return ort;
+        }
+    }
+}
